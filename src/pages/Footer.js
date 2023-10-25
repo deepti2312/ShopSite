dd1a1812a0963d8c085d035f8b47f405f5d876ba
deepti2312 <devishwakarma@bestpeers.in>
2023-10-25 19:02:25 +0530

    modified css

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,14 +1,13 @@
 import './Footer.css'
 import React from "react";
-import { Link } from "react-router-dom";
-
 
 const Footer = () => {
     return (
-        <>  
+        <>
             <div className="footer">
-                    <Link to="/" className="footerBtn"><i class="fa fa-home"></i></Link>
-                    <Link to="/" className="footerBtn"><i class="fa fa-search"></i></Link>
+                <p className='aboutus'>About us<br />
+                    footer@copyright.reserved
+                </p>
             </div>
         </>
     )
